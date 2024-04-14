587901ee361e2b61c7605bbcb93c61f1d290caa6
Arushi <arushi17@iastate.edu>
2024-04-14 11:16:46 -0500

    removed concept analysis folders

--- Concept Analysis/Experiments/attention_analysis.py
+++
@@ -1,8 +0,0 @@
-from utils import extract_attentions
-from run_neurox1 import MODEL_NAMES,MODEL_DESC
-
-for this_model in MODEL_NAMES:
-    if this_model in ['pretrained_GPT2']:
-        print(f"Generating the attentions file for {this_model}")
-        extract_attentions(MODEL_DESC[this_model],'./src_files/temp.in',)
-        break
